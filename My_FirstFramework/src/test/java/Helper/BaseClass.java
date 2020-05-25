452b3c55055d1ed5fb276c1771fd8dbca6686cbf
neeti.goyal <neeti.goyal@ddc2-l-7381kw2.dir.svc.accenture.com>
2020-05-25 16:45:47 +0530

    Extra imprts deleted

--- My_FirstFramework/src/test/java/Helper/BaseClass.java
+++ My_FirstFramework/src/test/java/Helper/BaseClass.java
@@ -1,25 +1,16 @@
 package Helper;
 
-import java.io.File;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-
 import com.MyInnovation.ObjectRepository.ElementFactory;
 import com.MyInnovation.Pages.Login;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-
 import Utilities.BrowserSelection;
 import Utilities.ExcelDataProvider;
-import Utilities.Screenshots;
 
 public class BaseClass {
 
--- My_FirstFramework/src/test/java/com/MyInnovation/Tests/Verify_Login.java
+++ My_FirstFramework/src/test/java/com/MyInnovation/Tests/Verify_Login.java
@@ -6,9 +6,6 @@ package com.MyInnovation.Tests;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
 import Helper.BaseClass;
